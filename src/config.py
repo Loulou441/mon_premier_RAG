ce7bee76baa33d415598a208eed4f48d1fcdb1b7
Manon Tessier <manon.tessier@etu.utc.fr>
2026-07-07 12:57:00 +0200

    feat: add configuration according to the subject

---
+++ src/config.py
@@ -0,0 +1,22 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()  # Charger les variables d'environnement depuis le fichier .env
+# Chemins de stockage
+DB_PATH = os.path.join(os.path.dirname(__file__), "chroma_db")
+PROMPTS_DIR = os.path.join(os.path.dirname(__file__), "prompts")
+
+# Modèles
+EMBEDDING_MODEL_NAME = "distiluse-base-multilingual-cased-v2" # Donné dans le sujet
+LLM_GENERATION_MODEL = "llama-3.3-70b-versatile"              # Donné dans le sujet
+LLM_MODERATION_MODEL = "llama-guard-3-8b"                     # Donné dans le sujet
+
+#Clé GROQ
+GROQ_API_KEY = os.getenv("GROQ_API_KEY")
+
+## Sécurité pour la clé GROQ
+if GROQ_API_KEY is None:
+    raise ValueError(
+        "GROQ_API_KEY introuvable. Vérifier son existance dans .env"
+        "et qu'il contient une ligne : GROQ_API_KEY=..."
+    )
